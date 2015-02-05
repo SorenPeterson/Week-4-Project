2c2a7684a899b1eaf35f7b927ca656d952968230
Soren Peterson <soren468@gmail.com>
2015-02-05 11:11:58 -0800

    migrate users and tweets

---
+++ db/migrate/20150205111035_create_tweets.rb
@@ -0,0 +1,11 @@
+class CreateTweets < ActiveRecord::Migration
+  def change
+    create_table :tweets do |t|
+      t.text :content
+      t.integer :user_id
+      t.integer :retweet_user_id
+
+      t.timestamps
+    end
+  end
+end

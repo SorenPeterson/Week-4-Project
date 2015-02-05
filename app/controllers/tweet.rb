f9ba0006068f20de90043f465f5c97754a3db8a9
Soren Peterson <soren468@gmail.com>
2015-02-05 11:32:35 -0800

    Add controllers

---
+++ app/controllers/tweet.rb
@@ -0,0 +1,29 @@
+# Display all tweets ever
+# Display all tweets ever
+get '/tweets' do
+end
+
+# Create a new tweet
+get '/tweets/new' do
+end
+
+# Create a new tweet
+post '/tweets' do
+
+end
+
+# Display a single tweet
+get '/tweets/:id' do
+end
+
+# Form to edit a tweet
+get '/tweets/:id/edit' do
+end
+
+# Modify a tweet
+put '/tweets/:id' do
+end
+
+# Delete a tweet
+delete '/tweets/:id' do
+end
---
+++ app/controllers/user.rb
@@ -0,0 +1,27 @@
+# Display a list of all users
+get '/users' do
+end
+
+# Sign up page
+get '/users/new' do
+end
+
+# Create a new user
+post '/users' do
+end
+
+# Display a users page with all of their tweets
+get '/users/:id' do
+end
+
+# Edit users preferences/settings
+get '/users/:id/edit' do
+end
+
+# Modify the users preferences
+put '/users/:id' do
+end
+
+# Delete a user
+delete '/users/:id' do
+end
